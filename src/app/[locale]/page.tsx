80cb3af45e0cf05419e673be55a5f9ebca32eaba
renanromagnollo <renanromagnollo@gmail.com>
2026-04-03 22:18:02 -0300

    feat: add Tailwind structure

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,7 +8,8 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>{locale}</h1>
+        <h1 className="text-3xl font-cursive">Language: {locale}</h1>
+        <h2>Caveat</h2>
 
       </main>
     </div>
